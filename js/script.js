fbba6bca0a159b9f8465240d52320a290035e123
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-01-20 17:39:19 +0100

    Milestone 4 completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,55 @@ new Vue({
         currentIndex: 0,
         writedMessage: "",
         myMessage: [],
+        search: "",
         contacts: [
+            {
+                name: 'Mario',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Ciao bello',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Come sta andando la vita?',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Non troppo bene, ho preso il covid',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Per fortuna sto bene e ho finito la quarantena',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Sono rimasto a casa due settimanen non ce la facevo più',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Che sfiga!',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Se vuoi uno di questi giorni andiamo a bere una birra',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Certo, volentieri',
+                        status: 'sent'
+                    },
+                ],
+            },
             {
                 name: 'Michele',
                 avatar: '_1',
@@ -89,6 +137,60 @@ new Vue({
                     }
                 ],
             },
+            // ---------------------------------
+            {
+                name: 'Mattia',
+                avatar: '_5',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Conosci Gianmarco Tocco?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei non conoscerlo',
+                        status: 'received'
+                    }
+                ],
+            },
+
+            {
+                name: 'Marta',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai visto il nuovo film?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'No, se vuoi andiamo insieme a vederlo',
+                        status: 'received'
+                    }
+                ],
+            },
+
+            {
+                name: 'Federico',
+                avatar: '_7',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Sei bravo a giocare a pallone?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Me la cavo gioco da quando sono piccolo',
+                        status: 'received'
+                    }
+                ],
+            },            
         ],
         
         
@@ -97,6 +199,7 @@ new Vue({
         indexClick: function (index) {
           this.currentIndex = index;
         },
+
         addUserMessage: function (index) {   //TODO mettere i messaggi in ordine verticale
             this.contacts[index].messages.push({
             text: this.writedMessage,
@@ -108,13 +211,21 @@ new Vue({
               this.autoReply(index)
           }, 1000)
         },
-        autoReply( index) {
+
+        autoReply(index) {
             this.contacts[index].messages.push({
                 date: "now",
                 text: "Ok",
                 status:"received",
             })
-        }
+        },
+
+        findAChat: function (contact) {
+            if (contact.name.toLowerCase().startsWith(this.search)) {
+              return true;
+            }
+            return false;
+          },
 
     }
 });
